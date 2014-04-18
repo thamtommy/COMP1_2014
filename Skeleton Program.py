4707c13888d6aab1163c094bd1d183c9c5a89565
Tommy T <tommytham@hotmail.co.uk>
2014-04-18 12:55:56 +0100

    Question 4
    Task 4 - Formatting the recent score table

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -170,8 +170,9 @@ def DisplayRecentScores(RecentScores):
   print()
   print('Recent Scores: ')
   print()
+  print("{0:<15}{1:<15}".format("Name","Score"))
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    print(RecentScores[Count].Name, 'got a score of', RecentScores[Count].Score)
+    print("{0:<15}{1:<15}".format(RecentScores[Count].Name,RecentScores[Count].Score))
   print()
   print('Press the Enter key to return to the main menu')
   input()
